60e1f6ccc29bf1c9e70a2ef52ece558332b9e26c
JPSS14 <jp_scarabelli@hotmail.com>
2021-12-02 23:48:59 -0300

    Add material-ui v5

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { Button } from "@mui/material";
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+
 export const App = () => {
   return (
     <div className="App">
       <h1>Olá Mundo!</h1>
+      <Button>
+        <AcUnitIcon/>
+        Testando
+      </Button>
     </div>
   );
 };
