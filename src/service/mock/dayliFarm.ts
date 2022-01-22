2ff25d94632b9413bb8940b80a1d9a4349b94b57
JPSS14 <jp_scarabelli@hotmail.com>
2022-01-22 20:41:30 -0300

    Refactor dayliFarm

---
+++ src/service/mock/dayliFarm.ts
@@ -0,0 +1,15 @@
+interface DayliFarmProps {
+  crystals: number;
+  credits: number;
+  shipCredits: number;
+  omega: number;
+  zeta: number;
+}
+
+export const dayliFarm: DayliFarmProps = {
+  crystals: 160,
+  credits: 647200,
+  shipCredits: 139400,
+  omega: 1,
+  zeta: 0.8
+}
