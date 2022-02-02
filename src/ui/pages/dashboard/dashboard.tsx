b29d31976c65272499868f48c23747de9d4682aa
JPSS14 <jp_scarabelli@hotmail.com>
2022-02-01 23:09:51 -0300

    Add register component in dashboard

--- src/ui/pages/dashboard/dashboard.tsx
+++ src/ui/pages/dashboard/dashboard.tsx
@@ -1,11 +1,29 @@
+import { Grid } from '@mui/material';
 import { BoxCt } from 'ui/components/box-ct';
 import { Layout } from 'ui/components/layout';
+import { Register } from 'ui/components/register';
 
 export const Dashboard = () => {
-  return(
+  return (
     <Layout>
       <BoxCt title='Farm Diário'>
-        sdssw
+        <Grid container rowSpacing={0}>
+          <Grid item xs={2}>
+            <Register title='Créditos' value={40} />
+          </Grid>
+          <Grid item xs={2}>
+            <Register title='Cristais' value={40} />
+          </Grid>
+          <Grid item xs={2}>
+            <Register title='Créditos de Naves' value={40} />
+          </Grid>
+          <Grid item xs={2}>
+            <Register title='Omega' value={40} />
+          </Grid>
+          <Grid item xs={2}>
+            <Register title='Zeta' value={40} />
+          </Grid>
+        </Grid>
       </BoxCt>
     </Layout>
   );
